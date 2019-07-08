7dcdf660085e34e047d718b4bbcb44d2a1f10dc5
Kamil Kołodziejczyk <kolodziejczykk@tt.com.pl>
2019-07-08 11:17:02 +0200

    Init frontend and backend part of project.

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+  console.log('FATAL ERROR: jwtPrivateKey is not defined');
+  process.exit(1);
+}
+
+mongoose
+  .connect('mongodb://localhost/blogging_me', {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('Could not connect to MongoDB with error: ', err));
+
+app.use(express.json());
+
+const port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+  console.log(`App start listening on ${port} port...`);
+});
