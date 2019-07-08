92cffef7cf650df2e2c8425fce1e9787d90cdfa1
Kamil Kołodziejczyk <kolodziejczykk@tt.com.pl>
2019-07-08 14:31:10 +0200

    Create template login page, add sass to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import LoginPage from './components/login-page/loginPage';
+import './App.scss';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <Switch>
+        <Route exact path="/" render={() => <LoginPage />} />
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/components/login-page/loginPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class LoginPage extends Component {
+  render() {
+    return <div>Login</div>;
+  }
+}
+
+export default withRouter(LoginPage);
