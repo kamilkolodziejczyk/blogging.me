71c895ec5726dee038b0be8016cfa7c22254ce88
Kamil Kołodziejczyk <kolodziejczykk@tt.com.pl>
2019-07-09 09:31:21 +0200

    Add not found page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import LoginPage from './components/login-page/loginPage';
 import RegisterPage from './components/register-page/registerPage';
 import HomePage from './components/home-page/homePage';
+import NotFound from './components/common/notFound';
 import './App.scss';
 import 'antd/dist/antd.css';
 
@@ -13,6 +14,8 @@ function App() {
         <Route exact path='/' render={() => <LoginPage />} />
         <Route path='/register' render={() => <RegisterPage />} />
         <Route path='/home' render={() => <HomePage />} />
+        <Route path='/not-found' render={() => <NotFound />} />
+        <Redirect to='/not-found' />
       </Switch>
     </div>
   );
